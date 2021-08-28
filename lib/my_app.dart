7063bbee7fe42dddba27f9f8d05b26548a14bf1f
Aaron Kelton <6170077+aaronkelton@users.noreply.github.com>
2021-08-28 11:37:30 -0500

    refactored the main app and routes
    * MaterialApp (in MyApp) is next to main.dart
    * pages/routes are in the views folder

---
+++ lib/my_app.dart
@@ -0,0 +1,15 @@
+import 'package:auto_route_2_3_0/views/first_route.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FirstRoute(),
+    );
+  }
+}
---
+++ lib/views/first_route.dart
@@ -0,0 +1,24 @@
+import 'package:auto_route_2_3_0/views/second_route.dart';
+import 'package:flutter/material.dart';
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Open route'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
